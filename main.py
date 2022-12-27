c35206cd44c3088ccc2fc7834d33c0689e5ef8a6
Nandan17 <vgnandan02@gmail.com>
2022-12-27 15:22:31 +0530

    updated main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("Hi there from Nandan")
+
+
 
 
 # Press the green button in the gutter to run the script.
