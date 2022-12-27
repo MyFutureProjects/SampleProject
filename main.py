e98f6d6a019126842429cd64b24727a46c86cd2f
Nandan17 <60395733+Nandan17@users.noreply.github.com>
2022-12-27 18:12:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("This is Nandan")
     print("Hi from add-caption-Nandan")
+    print("Hi")
 
 
 # Press the green button in the gutter to run the script.
